e95e48e567ab7b5438a6e7087357c2e54200b685
Erdős Dániel <daniel.erdos@doclerholding.com>
2017-04-19 16:16:54 +0200

    initial commit

---
+++ TestRunner/Options.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using CommandLine;
+using CommandLine.Text;
+
+namespace TestRunner
+{
+    public class Options
+    {
+
+        [Option( 'n', "nunit", Required = true, HelpText = "This is to set the nunit3 path and to run your tests" )]
+        public string NunitPathArg { get; set; }
+        [Option( 'd', "dll", Required = true, HelpText = "This is to specify, which dll we would like to run" )]
+        public string DllPathArg { get; set; }
+        [Option( 'o', "output", Required = false, HelpText = "You can set a filepath where you would like to save your text output file." )]
+        public string OutputOptionArg { get; set; }
+        [Option( 'b', "browser", Required = false, HelpText = "This will determine on which browser should the tests run. You can use: chrome, firefox, edge, safari, ie for the browser type parameter." )]
+        public string SelectedBrowserArg { get; set; }
+        [Option( 'r', "result", Required = false, HelpText = "You can set the file path for the NUnit generated xml and other reports from your tests. For example: --result= \"TestResult.xml;format=nunit2\"" )]
+        public string ResultOptionArg { get; set; }
+        [Option( 'w', "workers", Required = false, HelpText = "If parallelization is set in the AssemblyInfo file, you can set how many parallel threads you'd like to run your tests on." )]
+        public string ParallelWorkersOptionArg { get; set; }
+        [Option( 'h', "help", Required = false)]
+        public bool HelpAsked { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            HelpText lHelp = new HelpText {
+              AdditionalNewLineAfterOption = true,
+              AddDashesToOption = true
+            };
+
+            lHelp.Heading = new HeadingInfo( "TestRunner", "v1.0" );
+
+            lHelp.Copyright = new CopyrightInfo( "TestSquad", 2017 );
+
+            lHelp.AddPreOptionsLine( "This command line application is for help you running your NUnit based C# automation tests with custom parameters." );
+
+            lHelp.AddPreOptionsLine( Environment.NewLine );
+
+            lHelp.AddPreOptionsLine( "The application adds the option to set browser type based on command line argument, and writes out to a file, which can later read by your test setup." );
+
+            lHelp.AddPreOptionsLine( "For see additional help information, please run: TestRunner.exe --help" );
+
+            lHelp.AddPreOptionsLine( "Minimum usage is: Testrunner.exe -n/--nunit [path_to_nunit3-console.exe] -d/--dll [path_to_your_test_dll]" );
+
+            lHelp.AddPreOptionsLine( "You can also set text output (-o/--output), xml results (-r/--result), workers number (-w/--workers), browser (-b/--browser), but these are optional parameters" );
+
+            lHelp.AddOptions( this );
+
+            if ( LastParserState?.Errors.Any( ) == true )
+            {
+              string lErrors = lHelp.RenderParsingErrorsText(this, 2); // indent with two spaces
+
+              if ( !string.IsNullOrEmpty( lErrors ) )
+              {
+                lHelp.AddPreOptionsLine( string.Concat( Environment.NewLine, "ERROR(S):") );
+
+                lHelp.AddPreOptionsLine( lErrors );
+              }
+            }
+
+            return lHelp;
+        }
+    }
+}
---
+++ TestRunner/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CommandLine;
+
+namespace TestRunner
+{
+    public class Program
+    {
+        protected Program( ) { }
+
+        public static string mNunitPath { get; set; }
+        public static string mDllPath { get; set; }
+        public static string mBrowserType { get; set; }
+        public static string mOutPutOption { get; set; }
+        public static string mResultOption { get; set; }
+        public static string mWorkersNumber { get; set; }
+
+        private static void SetBrowserTypeInConfigTextFile( string aBrowserType )
+        {
+            string lCurrentBrowserType = aBrowserType == null ? "chrome" : aBrowserType.ToLowerInvariant( );
+
+            if ( mDllPath != null )
+            {
+                int lIndexOfLastPathSeparatorChar = mDllPath.LastIndexOf( Path.DirectorySeparatorChar );
+
+                if ( lIndexOfLastPathSeparatorChar > -1 )
+                {
+                    string lTestDllFolderPath = mDllPath.Substring( 0, lIndexOfLastPathSeparatorChar + 1 );
+
+                    if ( lTestDllFolderPath != null )
+                    {
+                        string lConfigFilePath = Path.Combine( lTestDllFolderPath, "config.txt" );
+
+                        File.WriteAllText( lConfigFilePath, lCurrentBrowserType );
+                    }
+                }
+            }
+        }
+
+        static void Main( string[ ] args )
+        {
+
+            Options lOptions = new Options( );
+            List<string> lAllArguments = new List<string>( );
+
+            if ( Parser.Default.ParseArguments( args, lOptions ) )
+            {
+                mNunitPath      = lOptions.NunitPathArg;
+                mDllPath        = lOptions.DllPathArg;
+                mBrowserType    = lOptions.SelectedBrowserArg;
+                mWorkersNumber  = lOptions.ParallelWorkersOptionArg;
+                mResultOption   = lOptions.ResultOptionArg;
+                mOutPutOption   = lOptions.OutputOptionArg;
+
+                lAllArguments.Add( mNunitPath       );
+                lAllArguments.Add( mDllPath         );
+                lAllArguments.Add( mBrowserType     );
+                lAllArguments.Add( mWorkersNumber   );
+                lAllArguments.Add( mResultOption    );
+                lAllArguments.Add( mOutPutOption    );
+            }
+            else if ( lOptions.HelpAsked )
+            {
+                // Display the default usage information
+                Console.WriteLine( lOptions.GetUsage( ) );
+            }
+
+            IEnumerable<string> lArgumentsWithValues = lAllArguments.Where( x => x != null );
+
+            int lArgumentsWithValuesCount = lArgumentsWithValues.Count( );
+
+            if ( lArgumentsWithValuesCount == 2 )
+            {
+                string lArgumentsForNunit = " --noresult " + mDllPath;
+
+                Process.Start( mNunitPath, lArgumentsForNunit );
+            }
+            else if ( lArgumentsWithValuesCount > 2 )
+            {
+                SetBrowserTypeInConfigTextFile( mBrowserType );
+
+                StringBuilder lCommandBuilder = new StringBuilder( );
+
+                if ( mResultOption == null && mOutPutOption == null )
+                {
+                    lCommandBuilder.Append( " --noresult" );
+                }
+
+                foreach( string lCurrentArg in lArgumentsWithValues )
+                {
+                    if ( lCurrentArg == mWorkersNumber )
+                    {
+                        lCommandBuilder.Append( $" --workers={lCurrentArg}" );
+                    }
+                    else if ( lCurrentArg == mOutPutOption )
+                    {
+                        lCommandBuilder.Append( $@" --out={lCurrentArg}" );
+                    }
+                    else if ( lCurrentArg == mResultOption )
+                    {
+                        lCommandBuilder.Append( $@" --result={lCurrentArg}" );
+                    }
+                }
+
+                lCommandBuilder.Append( $@" {mDllPath}" );
+
+                string lArgumentsForNunit = lCommandBuilder.ToString( );
+
+                Process.Start( mNunitPath, lArgumentsForNunit );
+
+            }
+        }
+    }
+}
